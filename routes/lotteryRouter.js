2f2f3ca052b52b800ae015c6a5f37d5ea50ddf57
Sujit Kumar <mailatsujit@gmail.com>
2020-06-22 10:30:50 +0530

    Update lotteryRouter.js

--- routes/lotteryRouter.js
+++ routes/lotteryRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 const controller = require("../controller/lotteryController");
 
 router.post("/createLottery", controller.postCreateLottery);
